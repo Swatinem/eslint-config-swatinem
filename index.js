b00e88098794b6f5be6232163e5c42e03c0d55ec
Arpad Borsos <arpad.borsos@googlemail.com>
2015-09-21 19:34:19 +0200

    Release 1.0.1

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const fs = require("fs")
 const path = require("path")
 const content = fs.readFileSync(path.resolve(__dirname, ".eslintrc"), "utf8")
 
-module.exports = Function("return " + content)()
+const config = Function(`return ${content}`)()
+const xo = require("eslint-config-xo")
+
+Object.keys(xo).forEach(key => {
+  xo[key] = Object.assign(xo[key], config[key])
+})
+
+module.exports = xo

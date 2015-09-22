e36d609899e53df1cc1a1d2feecbb16f748c78dc
Arpad Borsos <arpad.borsos@googlemail.com>
2015-09-22 14:06:08 +0200

    dont use ES6 features, since not everyone has node 4 yet

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const content = fs.readFileSync(path.resolve(__dirname, ".eslintrc"), "utf8")
 const config = Function(`return ${content}`)()
 const xo = require("eslint-config-xo")
 
-Object.keys(xo).forEach(key => {
-  xo[key] = Object.assign(xo[key], config[key])
+const exports = module.exports = Object.create(null)
+
+Object.keys(xo).forEach(function (key) {
+  exports[key] = assign(Object.create(null), xo[key], config[key])
 })
 
-module.exports = xo
+function assign(target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var obj = arguments[i]
+    if (!obj) { continue }
+    Object.keys(obj).forEach(function (key) {
+      target[key] = obj[key]
+    })
+  }
+  return target
+}

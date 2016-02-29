96ffdd75327df4dba84a81c0d1e92066aa5af922
Arpad Borsos <arpad.borsos@googlemail.com>
2016-02-29 11:34:22 +0100

    update to eslint2 and latest xo

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 const fs = require("fs")
 const path = require("path")
 const content = fs.readFileSync(path.resolve(__dirname, ".eslintrc"), "utf8")
-
 const config = Function(`return ${content}`)()
-const xo = require("eslint-config-xo")
 
-exports = module.exports = Object.create(null)
+config.extends = require.resolve("eslint-config-xo")
 
-Object.keys(xo).forEach(function (key) {
-  exports[key] = assign(Object.create(null), xo[key], config[key])
-})
-
-function assign(target) {
-  for (var i = 1; i < arguments.length; i++) {
-    var obj = arguments[i]
-    if (!obj) { continue }
-    Object.keys(obj).forEach(function (key) {
-      target[key] = obj[key]
-    })
-  }
-  return target
-}
+module.exports = config
